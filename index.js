38948e4d663cc392c653e9546db931b52774761b
1diamond11 <treldsher@gmail.com>
2025-03-05 09:32:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+console.log(`Имя: ${process.env.FIRST_NAME}`);
+console.log(`Фамилия: ${process.env.LAST_NAME}`);
+console.log(`Группа: ${process.env.GROUP_NUMBER}`);
+console.log(`Номер в списке: ${process.env.LIST_NUMBER}`);
